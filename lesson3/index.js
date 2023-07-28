77b8a48d5ffaf498c58d6135b0a82a3b344d190a
Oleg Razin <1greenfest@gmail.com>
2023-07-29 00:07:02 +0500

    Fixed few errors

--- lesson3/index.js
+++ lesson3/index.js
@@ -8,11 +8,14 @@ function checkMovie(guessFavoriteMovie) {
   if (guessFavoriteMovie !== favoriteMovie) {
     alert("Попробуйте еще раз")
   }
+  if (guessFavoriteMovie === favoriteMovie) {
+    alert("Правильно!")
+  }
 }
 
 // Task2
 let arr = ['a', 'b', 'c']
 arr[0] = "word"
-arr[1] = "sentence"
+arr[1] = "this is an example of sentence"
 arr[2] = "d"
 console.log(arr)
